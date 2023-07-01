e853b09ead1417ee4460145d71be4f063a69fd77
anishs <anish2sinha@gmail.com>
2023-07-01 13:21:56 -0400

    finished basic styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li>XXXX</li>
+        </ul>
+        <nav>
+          <p>Made by Anish</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1>ChatGPT</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+            Chat GPT Clone Practice Project
+          </p>
+        </div>
+      </section >
     </div>
   );
 }
